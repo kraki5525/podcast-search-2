174328ab10bbdfb2d47453fc672c14e98551969a
Richard McCollister <kraki5525@gmail.com>
2018-01-25 22:13:34 -0500

    rework logic for fetching description from table script

--- episode.js
+++ episode.js
@@ -1,14 +1,24 @@
 const cheerio = require('cheerio');
+const reg = /\{.*\}/;
 
 class Episode {
-    constructor(html, adamId) {
+    constructor(adamId, html) {
         const $ = cheerio.load(html);
         const selector = $(`tr[adam-id="${adamId}"]`)
+        const descriptionScript = $(`.track-list-inline-details div[adam-id="${adamId}"] script`).get(0).children[0].data;
+        const match = reg.exec(descriptionScript);
+        let description = null;
+        if (match) {
+            let jsonString = match[0];
+            description = jsonString;
+            // let json = JSON.parse(jsonString);
+            // description = json.description;
+        }
 
+        this.episodeDescription = description;
         this.adamId = adamId;
-        // this.episodeDescription = $('.name .text').text();
-        this.episodeReleaseDate = $('.release-date .text').text();
-        this.episodeTitle = $('.name .text').text();
+        this.episodeReleaseDate = selector.find('.release-date .text').text();
+        this.episodeTitle = selector.find('.name .text').text();
     }
 
     get description() {
