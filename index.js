d530a7f057731d55c3f6028daba23017cb11fd33
Richard McCollister <kraki5525@gmail.com>
2018-02-06 23:52:04 -0500

    start using process queue

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const AlphaPage = require('./alphaPage')
 const CategoryPage = require('./categoryPage')
 const PodcastPage = require('./podcastPage');
+const ProcessQueue = require('./processQueue');
 
 async function go(link, action) {
     const response = await axios.get(link);
@@ -23,22 +24,34 @@ function parsePodcast(html) {
 }
 
 async function main() {
-    const category = await go('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', parseCategory);
-    const alphaPagePromises = category.links
-        .map(link => go(link, parseAlpha));
-    const alphaPages = await Promise.all(alphaPagePromises);
-    const podcastLinksArrays = alphaPages
-        .map(alphaPage => alphaPage.links);
-    const podcastLinks = flatten(...podcastLinksArrays).slice(0, 10);
-    const podcastPagePromises = podcastLinks
-        .map(link => go(link, parsePodcast));
-    const podcastPages = await Promise.all(podcastPagePromises);
+    let queue = new ProcessQueue();
 
-    for (let x of podcastPages) {
-        if (x.title === "A.P. Government and Politics") {
-            console.log(x);
-        }
+    queue.add({url: 'https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', action: parseCategory})
+
+    for (let pages of queue.get()) {
+        let promises = pages.map(page => go(page.url, page.action));
+        let results = await Promise.all(promises);
+
+        queue.add()
     }
+
+    // const category = await go('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', parseCategory);
+    // const alphaPagePromises = category.links
+    //     .map(link => go(link, parseAlpha));
+    // const alphaPages = await Promise.all(alphaPagePromises);
+    // const podcastLinksArrays = alphaPages
+    //     .map(alphaPage => alphaPage.links);
+    // const podcastLinks = flatten(...podcastLinksArrays).slice(0, 10);
+    // const podcastPagePromises = podcastLinks
+    //     .map(link => go(link, parsePodcast));
+    // const podcastPages = await Promise.all(podcastPagePromises);
+
+    // for (let x of podcastPages) {
+    //     if (x.title === "A.P. Government and Politics") {
+    //         console.log(x);
+    //     }
+    // }
 }
 
+console.log('start');
 main();
