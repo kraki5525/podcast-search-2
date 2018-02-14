409736c9b413326d909d3a7fa0995c3a4fe575cf
Rich McCollister <kraki5525@gmail.com>
2018-02-14 13:09:36 -0500

    pushed process queue to action functions, added async sleep function

--- index.js
+++ index.js
@@ -5,34 +5,47 @@ const CategoryPage = require('./categoryPage')
 const PodcastPage = require('./podcastPage');
 const ProcessQueue = require('./processQueue');
 
-async function go(link, action) {
+async function go(link, action, queue) {
     const response = await axios.get(link);
 
-    return action(response.data);
-}
-
-function parseAlpha(html) {
-    return new AlphaPage(html);
-}
-
-function parseCategory(html) {
-    return new CategoryPage(html);
+    return action(response.data, queue);
 }
 
 function parsePodcast(html) {
     return new PodcastPage(html);
 }
 
+function parseAlpha(html, queue) {
+    const page = new AlphaPage(html);
+    for (let link of page.podcastLinks) {
+        queue.add({url: link, action: parsePodcast});
+    }
+}
+
+function parseCategory(html, queue) {
+    const page = new CategoryPage(html);
+    for (let link of page.alphaLinks) {
+        queue.add({url: link, action: parseAlpha});
+    }
+}
+
+function sleep(milliseconds) {
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds)
+    });
+}
+
 async function main() {
-    let queue = new ProcessQueue();
+    let queue = new ProcessQueue(4);
 
     queue.add({url: 'https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', action: parseCategory})
 
     for (let pages of queue.get()) {
-        let promises = pages.map(page => go(page.url, page.action));
-        let results = await Promise.all(promises);
+        console.log(`${new Date(Date.now())} -  ${pages.length}`)
+        let promises = pages.map(page => go(page.url, page.action, queue));
+        await Promise.all(promises);
 
-        queue.add()
+        await sleep(5000);
     }
 
     // const category = await go('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', parseCategory);
