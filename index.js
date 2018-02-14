e95e4aac16db02387d3a99e98e484c8fdcdfb021
Rich McCollister <kraki5525@gmail.com>
2018-02-14 14:34:38 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const {chunk, flatten} = require('lodash');
 const axios = require('axios');
-const AlphaPage = require('./alphaPage')
-const CategoryPage = require('./categoryPage')
+const AlphaPage = require('./alphaPage');
+const CategoryPage = require('./categoryPage');
 const PodcastPage = require('./podcastPage');
 const ProcessQueue = require('./processQueue');
 
@@ -31,39 +30,22 @@ function parseCategory(html, queue) {
 
 function sleep(milliseconds) {
     return new Promise(resolve => {
-        setTimeout(resolve, milliseconds)
+        setTimeout(resolve, milliseconds);
     });
 }
 
 async function main() {
     let queue = new ProcessQueue(4);
 
-    queue.add({url: 'https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', action: parseCategory})
+    queue.add({url: 'https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', action: parseCategory});
 
     for (let pages of queue.get()) {
-        console.log(`${new Date(Date.now())} -  ${pages.length}`)
+        console.log(`${new Date(Date.now())} -  ${pages.length}`);
         let promises = pages.map(page => go(page.url, page.action, queue));
         await Promise.all(promises);
 
         await sleep(5000);
     }
-
-    // const category = await go('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', parseCategory);
-    // const alphaPagePromises = category.links
-    //     .map(link => go(link, parseAlpha));
-    // const alphaPages = await Promise.all(alphaPagePromises);
-    // const podcastLinksArrays = alphaPages
-    //     .map(alphaPage => alphaPage.links);
-    // const podcastLinks = flatten(...podcastLinksArrays).slice(0, 10);
-    // const podcastPagePromises = podcastLinks
-    //     .map(link => go(link, parsePodcast));
-    // const podcastPages = await Promise.all(podcastPagePromises);
-
-    // for (let x of podcastPages) {
-    //     if (x.title === "A.P. Government and Politics") {
-    //         console.log(x);
-    //     }
-    // }
 }
 
 console.log('start');
