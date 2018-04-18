30b72b6db2b947ff437833a0e2ed95d0740a9e1c
Rich McCollister <kraki5525@gmail.com>
2018-04-18 14:28:57 -0400

    remove sync command types parameter usage

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const SyncCommand = require('./syncCommand');
 
 async function main() {
     const command = new SyncCommand();
-    // await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462?mt=2&letter=Q', 'Alpha');
-    await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462', 'Category');
+    // await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462?mt=2&letter=Q');
+    await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462');
 }
 
 main();
