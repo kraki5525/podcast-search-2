f9b81fdbec64e7b84c648d124bb941d02ceae099
Rich McCollister <kraki5525@gmail.com>
2018-04-20 12:25:49 -0400

    implement commander logic

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const program = require('commander');
 const SyncCommand = require('./commands/syncCommand');
 const MessageNotification = require('./messageNotification');
+const messageNotification = new MessageNotification();
 
+program
+    .command('sync [page]')
+    .action(async function(page) {
+        //'https://itunes.apple.com/us/genre/podcasts/id26?mt=2'
+        const command = new SyncCommand(messageNotification);
+        const url = page || 'https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462?mt=2&letter=X';
+        await command.execute(url);
+        console.log('done');
+    });
 
-async function main() {
-    const messageNotification = new MessageNotification();
-    const command = new SyncCommand(messageNotification);
-    // await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462?mt=2&letter=B');
-    await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462?mt=2&letter=A&page=2#page');
-    // await command.execute('https://itunes.apple.com/us/genre/podcasts-society-culture-history/id1462');
-}
-
-main();
+program.parse(process.argv);
